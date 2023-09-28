a7403d9a6a465ba9501441b330dbb7edf7cff391
JohanBarBBD <122454216+JohanBarBBD@users.noreply.github.com>
2023-09-28 17:54:28 +0200

    setup pa11y tests

---
+++ pa11y.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  defaults: {
+    // Pa11y configuration options go here
+  },
+  urls: [
+    // List of URLs to test
+    'http://localhost:3000',       // Test a local development server
+    'http://localhost:3000/quiz'
+  ],
+};
