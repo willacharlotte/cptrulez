6174020d8a274e53632d8e73cf3744a04a2924f9
JohanBarBBD <122454216+JohanBarBBD@users.noreply.github.com>
2023-09-28 17:57:36 +0200

    remove unnecessary comments and code

--- pa11y.config.js
+++ pa11y.config.js
@@ -1,10 +1,7 @@
 module.exports = {
-  defaults: {
-    // Pa11y configuration options go here
-  },
   urls: [
     // List of URLs to test
-    'http://localhost:3000',       // Test a local development server
-    'http://localhost:3000/quiz'
+    'http://localhost:3000',
+    'http://localhost:3000/quiz',
   ],
 };
